f4be4580eb97fae2ed5c250f8301dbb045c9a609
Conrad Hoang <conrad@sprqpt.com>
2025-01-04 09:08:32 -0500

    mdx might be a sin, should be switch back to markdoc 🤔

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import markdoc from '@astrojs/markdoc';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sprqpt.com',
-	integrations: [mdx(), sitemap(), svelte({ extensions: ['.svelte'] })],
+	integrations: [
+		mdx(),
+		markdoc(),
+		sitemap(),
+		svelte({ extensions: ['.svelte'] }),
+	],
 	experimental: {
 		svg: true,
 	},
