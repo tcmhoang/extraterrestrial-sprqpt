d4f5a126ea1ac9d22666f6c8ff7ed4bc0070fbb9
Conrad Hoang <conrad@sprqpt.com>
2025-02-08 00:42:28 -0500

    Use the same jibble jabber in the middleware 💫

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,19 @@
-export async function onRequest(
-	/** @type {import('npm:astro').APIContext} */ context,
-	/** @type {import('npm:astro').MiddlewareNext} */ next,
-) {
-	const response = await next();
-	const html = await response.text();
+const security_headers = {
+  "Cross-Origin-Embedder-Policy": "require-corp",
+  "Cross-Origin-Opener-Policy": "same-origin",
+  "X-XSS-Protection": "1; mode=block",
+  "X-Frame-Options": "SAMEORIGIN",
+  "X-Content-Type-Options": "nosniff",
+};
 
-	return new Response(html, {
-		status: 200,
-		headers: {
-			...response.headers,
-			'Cross-Origin-Embedder-Policy': 'require-corp',
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'X-XSS-Protection': '1; mode=block',
-			'X-Frame-Options': 'SAMEORIGIN',
-			'X-Content-Type-Options': 'nosniff',
-		},
-	});
+export async function onRequest(
+  /** @type {import('npm:astro').APIContext} */ _,
+  /** @type {import('npm:astro').MiddlewareNext} */ next
+) {
+  const response = await next();
+  Object.entries(security_headers).forEach(([header, value]) =>
+    response.headers.set(header, value)
+  );
+
+  return response;
 }
